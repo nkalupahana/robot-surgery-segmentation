7a65d98f530f28350e2b6e6a3ea86bb6f60c6591
nkalupahana <avishkank@gmail.com>
2022-09-05 20:09:23 -0500

    Remove unused stuff

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 from pathlib import Path
-from validation import validation_binary, validation_multi
+from validation import validation_binary
 
 import torch
 from torch import nn
@@ -10,8 +10,8 @@ from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
 import torch.backends.cudnn
 
-from models import UNet11, LinkNet34, UNet, UNet16, AlbuNet
-from loss import LossBinary, LossMulti
+from models import UNet11
+from loss import LossBinary
 from dataset import RoboticsDataset
 import utils
 import sys
@@ -26,13 +26,6 @@ from albumentations import (
     RandomCrop,
     CenterCrop
 )
-
-moddel_list = {'UNet11': UNet11,
-               'UNet16': UNet16,
-               'UNet': UNet,
-               'AlbuNet': AlbuNet,
-               'LinkNet34': LinkNet34}
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,8 +42,6 @@ def main():
     arg('--train_crop_width', type=int, default=1280)
     arg('--val_crop_height', type=int, default=1024)
     arg('--val_crop_width', type=int, default=1280)
-    arg('--type', type=str, default='binary', choices=['binary', 'parts', 'instruments'])
-    arg('--model', type=str, default='UNet', choices=moddel_list.keys())
 
     args = parser.parse_args()
 
@@ -69,18 +60,8 @@ def main():
               'are not.'.format(val_crop_height=args.val_crop_height, val_crop_width=args.val_crop_width))
         sys.exit(0)
 
-    if args.type == 'parts':
-        num_classes = 4
-    elif args.type == 'instruments':
-        num_classes = 8
-    else:
-        num_classes = 1
-
-    if args.model == 'UNet':
-        model = UNet(num_classes=num_classes)
-    else:
-        model_name = moddel_list[args.model]
-        model = model_name(num_classes=num_classes, pretrained=True)
+    num_classes = 1
+    model = UNet11(num_classes=num_classes, pretrained=True)
 
     if torch.cuda.is_available():
         if args.device_ids:
@@ -91,16 +72,13 @@ def main():
     else:
         raise SystemError('GPU device not found')
 
-    if args.type == 'binary':
-        loss = LossBinary(jaccard_weight=args.jaccard_weight)
-    else:
-        loss = LossMulti(num_classes=num_classes, jaccard_weight=args.jaccard_weight)
+    loss = LossBinary(jaccard_weight=args.jaccard_weight)
 
     cudnn.benchmark = True
 
-    def make_loader(file_names, shuffle=False, transform=None, problem_type='binary', batch_size=1):
+    def make_loader(file_names, shuffle=False, transform=None, batch_size=1):
         return DataLoader(
-            dataset=RoboticsDataset(file_names, transform=transform, problem_type=problem_type),
+            dataset=RoboticsDataset(file_names, transform=transform, problem_type='binary'),
             shuffle=shuffle,
             num_workers=args.workers,
             batch_size=batch_size,
@@ -127,18 +105,13 @@ def main():
             Normalize(p=1)
         ], p=p)
 
-    train_loader = make_loader(train_file_names, shuffle=True, transform=train_transform(p=1), problem_type=args.type,
-                               batch_size=args.batch_size)
-    valid_loader = make_loader(val_file_names, transform=val_transform(p=1), problem_type=args.type,
-                               batch_size=len(device_ids))
+    train_loader = make_loader(train_file_names, shuffle=True, transform=train_transform(p=1), batch_size=args.batch_size)
+    valid_loader = make_loader(val_file_names, transform=val_transform(p=1), batch_size=len(device_ids))
 
     root.joinpath('params.json').write_text(
         json.dumps(vars(args), indent=True, sort_keys=True))
 
-    if args.type == 'binary':
-        valid = validation_binary
-    else:
-        valid = validation_multi
+    valid = validation_binary
 
     utils.train(
         init_optimizer=lambda lr: Adam(model.parameters(), lr=lr),
